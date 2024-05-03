ccf6961a0722d4aa661df64dff94987357db8ec3
trezor2 <ckwo873@aucklanduni.ac.nz>
2024-05-04 00:00:19 +1200

    fix
    Edited router and DON addresses for Base Sepolia

--- packages/contracts/contracts/ApiCallOracle.sol
+++ packages/contracts/contracts/ApiCallOracle.sol
@@ -31,9 +31,8 @@ contract ApiCallOracle is FunctionsClient, ConfirmedOwner {
 		bytes err
 	);
 
-	// Router address - Hardcoded for Sepolia
-	// Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
-	address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
+	// Router address - Hardcoded for Base Sepolia
+	address router = 0xf9B8fc078197181C841c296C876945aaa425B278;
 
 	// JavaScript source code
 	// Fetch character name from the Star Wars API.
@@ -52,10 +51,9 @@ contract ApiCallOracle is FunctionsClient, ConfirmedOwner {
 	//Callback gas limit
 	uint32 gasLimit = 300000;
 
-	// donID - Hardcoded for Sepolia
-	// Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+	// donID - Hardcoded for Base Sepolia
 	bytes32 donID =
-		0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+		0x66756e2d626173652d7365706f6c69612d310000000000000000000000000000;
 
 	// State variable to store the returned character information
 	string public character;
