315862d1d05de0378c594bd2b18ff8dbb5bd5e5c
Morgan Ross <morgan.ross@developersinstitute.co.nz>
2024-05-05 03:52:35 +1200

    updated mobile layout for onboarding

--- packages/client/src/components/OnBoarding.tsx
+++ packages/client/src/components/OnBoarding.tsx
@@ -5,12 +5,15 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import MobileStepper from '@mui/material/MobileStepper';
+import Hidden from '@mui/material/Hidden';
 import LoginButton from './LoginButton';
 import WorldCoinSignIn from './WorldCoinSignIn';
 import Wallet from './Wallet';
 import SetLocation from './SetLocation';
 import GateFiBuy from './GateFiBuy';
 import BuyPolicy from './BuyPolicy';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
 
 const steps = [
   'Login',
@@ -26,7 +29,7 @@ const stepComponents = [
   <WorldCoinSignIn key={1} />,
   <Wallet key={2} />,
   <SetLocation key={3} />,
-  <BuyPolicy />,
+  <BuyPolicy key={4} />,
 ];
 
 // replace "steps" with "stepDetails" and then use the .label field to get the step label text and the .component field to get the component to display on the page.
@@ -86,27 +89,58 @@ export default function OnBoarding() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
+      <Hidden smUp>
+        <MobileStepper
+          variant="dots"
+          steps={6}
+          position="static"
+          activeStep={activeStep}
+          sx={{ maxWidth: 400, flexGrow: 1 }}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === 5}
+            >
+              Next
+              <KeyboardArrowRight />
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              <KeyboardArrowLeft />
+              Back
+            </Button>
+          }
+        />
+      </Hidden>
+      <Hidden smDown>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps: { completed?: boolean } = {};
+            const labelProps: {
+              optional?: React.ReactNode;
+            } = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
             );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
+          })}
+        </Stepper>
+      </Hidden>
       <Typography variant="h4" sx={{ mt: 2, mb: 1, textAlign: 'center' }}>
         Step {activeStep + 1}
       </Typography>
