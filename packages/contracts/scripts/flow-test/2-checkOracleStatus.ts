a8f1c2e2dc99f5717e2c0c54dab86d07845108b4
manjik-rumsan <manjik.shrestha@rumsan.com>
2024-05-04 23:06:44 +1000

    Add checkResponse function to fetch and log response, error, requestId, and error data from the API call oracle contract

--- packages/contracts/scripts/flow-test/2-checkOracleStatus.ts
+++ packages/contracts/scripts/flow-test/2-checkOracleStatus.ts
@@ -1,5 +1,6 @@
 
 import { getSigners, fetchContracts } from "./common"
+import { ethers } from "hardhat"
 
 async function checkIsVictim() {
     const { victim1 } = await getSigners()
@@ -7,10 +8,25 @@ async function checkIsVictim() {
     console.log("is address added: " + await apiCallOracle.isAddressAdded(victim1.address))
 }
 
+async function checkResponse() {
+    const { apiCallOracle } = await fetchContracts()
+    const response = await apiCallOracle.s_lastResponse()
+    const error = await apiCallOracle.s_lastError()
+    const requestId = await apiCallOracle.s_lastRequestId()
+    console.log("response: " + response)
+    console.log("error: " + error)
+    console.log("requestId: " + requestId)
+    const errData = ethers.decodeBase64(error)
+    console.log("error data: " + errData)
 
-checkIsVictim()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+}
+
+
+// checkIsVictim()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.error(error)
+//         process.exit(1)
+//     })
+
+checkResponse()
